cd040fe19acf8c026a1bade2aae4e89d2f91a312
Morfly <pavlo.stavytskyi@gmail.com>
2024-04-26 19:40:37 -0700

    update demo screen

--- app/src/main/java/io/morfly/composelayouts/ui/bottomsheet/Material3BottomSheetScreen.kt
+++ app/src/main/java/io/morfly/composelayouts/ui/bottomsheet/Material3BottomSheetScreen.kt
@@ -1,20 +1,28 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package io.morfly.composelayouts.ui.bottomsheet
 
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import io.morfly.composelayouts.ui.BottomSheetContent
 import io.morfly.composelayouts.ui.BottomSheetScreenBody
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Material3BottomSheetDemo() {
+    val sheetState = rememberStandardBottomSheetState(
+        initialValue = SheetValue.PartiallyExpanded
+    )
+    val scaffoldState = rememberBottomSheetScaffoldState(sheetState)
+
     BottomSheetScaffold(
-        sheetContent = {
-            BottomSheetContent()
-        },
-        content = {
-            BottomSheetScreenBody()
-        }
+        sheetPeekHeight = 56.dp,
+        scaffoldState = scaffoldState,
+        sheetContent = { BottomSheetContent() },
+        content = { BottomSheetScreenBody() },
     )
 }
