f162f8b4559e6af85fd60c913614b3c8b370e2b3
Morfly <pavlo.stavytskyi@gmail.com>
2024-04-26 20:05:41 -0700

    rename theme

--- app/src/main/kotlin/io/morfly/bottomsheet/sample/MainActivity.kt
+++ app/src/main/kotlin/io/morfly/bottomsheet/sample/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
-import io.morfly.bottomsheet.sample.theme.ComposeLayoutsTheme
+import io.morfly.bottomsheet.sample.theme.MultiStateBottomSheetSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeLayoutsTheme {
+            MultiStateBottomSheetSampleTheme {
                 Surface {
                     Navigation()
                 }
