a81f742a5da06145108c27966387661c4b817e8e
Morfly <pavlo.stavytskyi@gmail.com>
2024-04-26 19:48:00 -0700

    update SheetValue

--- app/src/main/java/io/morfly/composelayouts/bottomsheet/custom/CustomBottomSheetScreen.kt
+++ app/src/main/java/io/morfly/composelayouts/bottomsheet/custom/CustomBottomSheetScreen.kt
@@ -13,22 +13,23 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
 import io.morfly.compose.bottomsheet.material3.BottomSheetScaffold
 import io.morfly.compose.bottomsheet.material3.rememberBottomSheetState
-import io.morfly.composelayouts.DragValue
 import io.morfly.composelayouts.bottomsheet.BottomSheetContent
 import io.morfly.composelayouts.bottomsheet.BottomSheetScreenBody
+
+private enum class SheetValue { Peek, PartiallyExpanded, Expanded }
 
 @Composable
 fun CustomBottomSheetScreen() {
     var isInitialState by rememberSaveable { mutableStateOf(true) }
 
     val state = rememberBottomSheetState(
-        initialValue = DragValue.Center,
+        initialValue = SheetValue.PartiallyExpanded,
         defineValues = {
-            DragValue.Start at height(56.dp)
+            SheetValue.Peek at height(56.dp)
             if (isInitialState) {
-                DragValue.Center at height(percent = 40)
+                SheetValue.PartiallyExpanded at height(percent = 40)
             }
-            DragValue.End at contentHeight
+            SheetValue.Expanded at contentHeight
         },
         confirmValueChange = {
             if (isInitialState) {
