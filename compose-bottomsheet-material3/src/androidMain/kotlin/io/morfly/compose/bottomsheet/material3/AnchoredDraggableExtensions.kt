6543a85dadc1779aabe344852afccdbf402306ef
Morfly <pavlo.stavytskyi@gmail.com>
2024-05-19 16:08:09 -0700

    add documentation for AnchoredDraggableExtensions.kt

--- compose-bottomsheet-material3/src/androidMain/kotlin/io/morfly/compose/bottomsheet/material3/AnchoredDraggableExtensions.kt
+++ compose-bottomsheet-material3/src/androidMain/kotlin/io/morfly/compose/bottomsheet/material3/AnchoredDraggableExtensions.kt
@@ -25,6 +25,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
+/**
+ * Updates the anchors similarly to [AnchoredDraggableState.updateAnchors] but animates the
+ * transition instead of snapping in case the [AnchoredDraggableState.currentValue] is not present
+ * anymore.
+ *
+ * @param scope the [CoroutineScope] to be used for the animation
+ * @param newAnchors the new anchors
+ * @param newTarget the new target value
+ */
 @ExperimentalFoundationApi
 fun <T> AnchoredDraggableState<T>.updateAnchorsAnimated(
     scope: CoroutineScope,
@@ -40,6 +49,20 @@ fun <T> AnchoredDraggableState<T>.updateAnchorsAnimated(
     }
 }
 
+/**
+ * Create and [rememberSaveable] an [AnchoredDraggableState].
+ *
+ * @param initialValue The initial value of the state.
+ * @param positionalThreshold The positional threshold, in px, to be used when calculating the
+ * target state while a drag is in progress and when settling after the drag ends. This is the
+ * distance from the start of a transition. It will be, depending on the direction of the
+ * interaction, added or subtracted from/to the origin offset. It should always be a positive value.
+ * @param velocityThreshold The velocity threshold (in px per second) that the end velocity has to
+ * exceed in order to animate to the next state, even if the [positionalThreshold] has not been
+ * reached.
+ * @param animationSpec The default animation that will be used to animate to a new state.
+ * @param confirmValueChange Optional callback invoked to confirm or veto a pending state change.
+ */
 @ExperimentalMaterial3Api
 @ExperimentalFoundationApi
 @Composable
