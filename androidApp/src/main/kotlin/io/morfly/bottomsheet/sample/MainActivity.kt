8dd66a9c47322aff4b922999052644fb86f24d51
Morfly <pavlo.stavytskyi@gmail.com>
2024-05-19 21:05:26 -0700

    fix status bar to respect dark/light mode

--- androidApp/src/main/kotlin/io/morfly/bottomsheet/sample/MainActivity.kt
+++ androidApp/src/main/kotlin/io/morfly/bottomsheet/sample/MainActivity.kt
@@ -1,7 +1,9 @@
 package io.morfly.bottomsheet.sample
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
@@ -10,7 +12,7 @@ import io.morfly.bottomsheet.sample.theme.MultiStateBottomSheetSampleTheme
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        enableEdgeToEdgeWithUiMode()
         super.onCreate(savedInstanceState)
         setContent {
             MultiStateBottomSheetSampleTheme {
@@ -20,4 +22,19 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+private fun ComponentActivity.enableEdgeToEdgeWithUiMode() {
+    val uiMode = resources.configuration.uiMode
+    val isDark = (uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+
+    val statusBarStyle = if (isDark) {
+        SystemBarStyle.dark(android.graphics.Color.TRANSPARENT)
+    } else {
+        SystemBarStyle.light(
+            android.graphics.Color.TRANSPARENT,
+            android.graphics.Color.TRANSPARENT
+        )
+    }
+    enableEdgeToEdge(statusBarStyle = statusBarStyle)
 }
